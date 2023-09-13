d5ed0a3f054c3f3e438e2d4e4c5f4b9ab0458c58
Merc5Dark <31131794siya@gmail.com>
2023-09-13 15:22:32 +0200

    Update 02. data-types.py

--- 02. data-types.py
+++ 02. data-types.py
@@ -2,7 +2,7 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Round the result to 2 decimal places.
 print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
+bill = float(input("What was the total bill? Zar "))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill?"))
 
